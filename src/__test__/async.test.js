3f8e8cf419b4bb826648bbd70a30770d16a1fadd
rahul <reticentrahul@gmail.com>
2022-08-17 18:05:07 +0530

    update: fake timers implemented

--- src/__test__/async.test.js
+++ src/__test__/async.test.js
@@ -19,25 +19,26 @@ it("test async functions", () => {
   });
 });
 
+
 test("test async functions with async await", async () => {
   await expect(fetchData()).resolves.toMatchObject(
     expect.objectContaining({
       data: { name: expect.any(String), value: expect.any(Number) },
       message: expect.any(String),
     })
-  );
-});
-
-test("test async functions with rejection", async () => {
+    );
+  });
+  
+  test("test async functions with rejection", async () => {
   expect.assertions(1);
   await expect(fetchData(true)).rejects.toMatchObject(
     expect.objectContaining({
       message: expect.any(String),
     })
-  );
-});
-
-// done function is required to be called before moving to next test case
+    );
+  });
+  
+  // done function is required to be called before moving to next test case
 test("test async functions with callbacks", (done) => {
   function cb(error, data) {
     if (error) {
@@ -53,3 +54,33 @@ test("test async functions with callbacks", (done) => {
   }
   fetchMsg(cb);
 });
+
+describe("with fake timers", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
+  })
+
+  afterAll(() => {
+    jest.useRealTimers();
+    jest.restoreAllMocks();
+  })
+
+  test("test async functions with fake timers", () => {
+    fetchData();
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+  });
+
+  test("test with callback with fake timer", () => {
+    const cb=jest.fn();
+    fetchMsg(cb);
+    expect(setTimeout).toHaveBeenCalledTimes(2);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+
+    jest.runAllTimers();
+    
+    expect(cb).toBeCalled();
+    expect(cb).toHaveBeenCalledWith(null, { message: 'completed' });
+  })
+})
